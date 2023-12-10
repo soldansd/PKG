a305016e5383d3d0956c2dee836d426a3de0f1a6
Daniil <137879572+soldansd@users.noreply.github.com>
2023-12-10 21:21:42 +0300

    Add features

---
+++ PKG_Lab3/exe/moc_mainwindow.cpp
@@ -0,0 +1,292 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../PKG_Lab3/mainwindow.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.5.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
+    "MainWindow",
+    "chooseFileSlot",
+    "",
+    "addConstantSlot",
+    "negativeSlot",
+    "mulConstantSlot",
+    "degreeSlot",
+    "logarithmSlot",
+    "addSlot",
+    "mulSlot",
+    "linearContrastSlot",
+    "erosionSlot",
+    "dilatationSlot",
+    "trippingSlot",
+    "lockingSlot",
+    "squareSlot",
+    "rectHorizontalSlot",
+    "rectVerticalSlot",
+    "crossSlot"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
+    uint offsetsAndSizes[38];
+    char stringdata0[11];
+    char stringdata1[15];
+    char stringdata2[1];
+    char stringdata3[16];
+    char stringdata4[13];
+    char stringdata5[16];
+    char stringdata6[11];
+    char stringdata7[14];
+    char stringdata8[8];
+    char stringdata9[8];
+    char stringdata10[19];
+    char stringdata11[12];
+    char stringdata12[15];
+    char stringdata13[13];
+    char stringdata14[12];
+    char stringdata15[11];
+    char stringdata16[19];
+    char stringdata17[17];
+    char stringdata18[10];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_stringdata_CLASSMainWindowENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 14),  // "chooseFileSlot"
+        QT_MOC_LITERAL(26, 0),  // ""
+        QT_MOC_LITERAL(27, 15),  // "addConstantSlot"
+        QT_MOC_LITERAL(43, 12),  // "negativeSlot"
+        QT_MOC_LITERAL(56, 15),  // "mulConstantSlot"
+        QT_MOC_LITERAL(72, 10),  // "degreeSlot"
+        QT_MOC_LITERAL(83, 13),  // "logarithmSlot"
+        QT_MOC_LITERAL(97, 7),  // "addSlot"
+        QT_MOC_LITERAL(105, 7),  // "mulSlot"
+        QT_MOC_LITERAL(113, 18),  // "linearContrastSlot"
+        QT_MOC_LITERAL(132, 11),  // "erosionSlot"
+        QT_MOC_LITERAL(144, 14),  // "dilatationSlot"
+        QT_MOC_LITERAL(159, 12),  // "trippingSlot"
+        QT_MOC_LITERAL(172, 11),  // "lockingSlot"
+        QT_MOC_LITERAL(184, 10),  // "squareSlot"
+        QT_MOC_LITERAL(195, 18),  // "rectHorizontalSlot"
+        QT_MOC_LITERAL(214, 16),  // "rectVerticalSlot"
+        QT_MOC_LITERAL(231, 9)   // "crossSlot"
+    },
+    "MainWindow",
+    "chooseFileSlot",
+    "",
+    "addConstantSlot",
+    "negativeSlot",
+    "mulConstantSlot",
+    "degreeSlot",
+    "logarithmSlot",
+    "addSlot",
+    "mulSlot",
+    "linearContrastSlot",
+    "erosionSlot",
+    "dilatationSlot",
+    "trippingSlot",
+    "lockingSlot",
+    "squareSlot",
+    "rectHorizontalSlot",
+    "rectVerticalSlot",
+    "crossSlot"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
+
+ // content:
+      11,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      17,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  116,    2, 0x0a,    1 /* Public */,
+       3,    0,  117,    2, 0x0a,    2 /* Public */,
+       4,    0,  118,    2, 0x0a,    3 /* Public */,
+       5,    0,  119,    2, 0x0a,    4 /* Public */,
+       6,    0,  120,    2, 0x0a,    5 /* Public */,
+       7,    0,  121,    2, 0x0a,    6 /* Public */,
+       8,    0,  122,    2, 0x0a,    7 /* Public */,
+       9,    0,  123,    2, 0x0a,    8 /* Public */,
+      10,    0,  124,    2, 0x0a,    9 /* Public */,
+      11,    0,  125,    2, 0x0a,   10 /* Public */,
+      12,    0,  126,    2, 0x0a,   11 /* Public */,
+      13,    0,  127,    2, 0x0a,   12 /* Public */,
+      14,    0,  128,    2, 0x0a,   13 /* Public */,
+      15,    0,  129,    2, 0x0a,   14 /* Public */,
+      16,    0,  130,    2, 0x0a,   15 /* Public */,
+      17,    0,  131,    2, 0x0a,   16 /* Public */,
+      18,    0,  132,    2, 0x0a,   17 /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSMainWindowENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSMainWindowENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'chooseFileSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'addConstantSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'negativeSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'mulConstantSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'degreeSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'logarithmSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'addSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'mulSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'linearContrastSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'erosionSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'dilatationSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'trippingSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'lockingSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'squareSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'rectHorizontalSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'rectVerticalSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'crossSlot'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->chooseFileSlot(); break;
+        case 1: _t->addConstantSlot(); break;
+        case 2: _t->negativeSlot(); break;
+        case 3: _t->mulConstantSlot(); break;
+        case 4: _t->degreeSlot(); break;
+        case 5: _t->logarithmSlot(); break;
+        case 6: _t->addSlot(); break;
+        case 7: _t->mulSlot(); break;
+        case 8: _t->linearContrastSlot(); break;
+        case 9: _t->erosionSlot(); break;
+        case 10: _t->dilatationSlot(); break;
+        case 11: _t->trippingSlot(); break;
+        case 12: _t->lockingSlot(); break;
+        case 13: _t->squareSlot(); break;
+        case 14: _t->rectHorizontalSlot(); break;
+        case 15: _t->rectVerticalSlot(); break;
+        case 16: _t->crossSlot(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSMainWindowENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 17)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 17;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 17)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 17;
+    }
+    return _id;
+}
+QT_WARNING_POP
